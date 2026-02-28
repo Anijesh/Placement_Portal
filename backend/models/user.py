ed5104ed9802eadc4841264bb56de6161d8d365f
Anijesh <anijeshkr@gmail.com>
2026-03-01 01:41:34 +0530

    user database model created

---
+++ backend/models/user.py
@@ -0,0 +1,18 @@
+from extensions import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key='True')
+    email = db.Column(db.String(100),unique = 'True',nullable='False')
+    password= db.Column(db.String(100),nullable ="False")
+    role= db.Column(db.String(25),nullable ='False',default='student')
+    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+    def check_password(self,password):
+        return check_password_hash(self.password, password)
+
