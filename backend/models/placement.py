6c457d3b64f14d97de97fd248e6d4a47025f94b5
Anijesh <anijeshkr@gmail.com>
2026-03-01 14:57:25 +0530

    placement database model created

---
+++ backend/models/placement.py
@@ -0,0 +1,13 @@
+from extensions import db
+
+class Placement(db.Model):
+    __tablename__ = "placements"
+
+    id = db.Column(db.Integer, primary_key=True)
+    application_id = db.Column(
+        db.Integer, db.ForeignKey("applications.id"), nullable=False, unique=True
+    )
+    offered_salary = db.Column(db.Float)
+    joining_date = db.Column(db.Date)
+
+    application = db.relationship("Application", backref="placement")
