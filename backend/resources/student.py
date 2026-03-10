5f1cff1f62594af47b5a86c6f6cbb441fc11c466
Anijesh <anijeshkr@gmail.com>
2026-03-11 02:35:19 +0530

    Add job application functionality for students

--- backend/resources/student.py
+++ backend/resources/student.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required, get_jwt
-from models import Student, Company, Job,User
+from flask_jwt_extended import jwt_required, get_jwt,get_jwt_identity
+from models import Student, Company, Job,User,Application
 from extensions import db
 
 class StudentJobList(Resource):
@@ -25,3 +25,25 @@ class StudentJobList(Resource):
                 "salary" : job.salary,
             })
         return result,200
+    
+class StudentApplyJob(Resource):
+    @jwt_required()
+    def post(self,id):
+        claims = get_jwt()
+        if claims.get('role') != 'student':
+            return {"message":"Student access required"},403
+        job = Job.query.get(id)
+        if not job:
+            return {"message": "Job not found"}, 404
+        if job.status != 'approved':
+            return {'message':"This drive is not in active status"},400
+        student = Student.query.filter_by(user_id=get_jwt_identity()).first()
+        existing = Application.query.filter_by(student_id=student.id,job_id=job.id).first()
+        if existing:
+            return {'message':"Already applied for Application"},400
+        application=Application(student_id = student.id,
+                                job_id = job.id)
+        db.session.add(application)
+        db.session.commit()
+        return {"message":"Application submitted successfully"},201
+        
--- backend/routes/student_bp.py
+++ backend/routes/student_bp.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.student import(
     StudentJobList,
+    StudentApplyJob,
 )
 
 student_bp=Blueprint("student_bp",__name__)
 api = Api(student_bp)
 
 api.add_resource(StudentJobList,'/jobs/list')
+api.add_resource(StudentApplyJob,'/job/<int:id>/apply')
