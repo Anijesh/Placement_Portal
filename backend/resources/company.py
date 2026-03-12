5e2f6d3c089f837228562e5ddd3ea2830197aac7
Anijesh <anijeshkr@gmail.com>
2026-03-13 00:51:39 +0530

    Add functionality to shortlist student job applications by company users

--- backend/resources/company.py
+++ backend/resources/company.py
@@ -77,3 +77,16 @@ class CompanyApplicatonList(Resource):
 
             })
         return result,200
+class CompanyShortlistApplication(Resource):
+    @jwt_required()
+    def put(self,id):
+        claims = get_jwt()
+        if claims.get('role') != 'company':
+            return {"message":"company access required"},403
+        company=Company.query.filter_by(user_id = get_jwt_identity()).first()
+        application = Application.query.get(id)
+        if not application:
+            return {'message':"Application not found"},404
+        application.status='shortlisted'
+        db.session.commit()
+        return {"message": "Student shortlisted"}, 200
--- backend/routes/company_bp.py
+++ backend/routes/company_bp.py
@@ -4,6 +4,7 @@ from resources.company import(
     CompanyCreateJob,
     CompanyJobList,
     CompanyApplicatonList,
+    CompanyShortlistApplication,
 )
 
 company_bp=Blueprint('company_bp',__name__)
@@ -12,3 +13,4 @@ api=Api(company_bp)
 api.add_resource(CompanyCreateJob,'/create/job')
 api.add_resource(CompanyJobList,'/job/list')
 api.add_resource(CompanyApplicatonList,'/job/application/<int:id>/list')
+api.add_resource(CompanyShortlistApplication,'/application/<int:id>/shortlist')
