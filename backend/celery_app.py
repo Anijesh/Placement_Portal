3e5928d5d707ab9f16cdba1b4c64cf62396625bb
Anijesh <anijeshkr@gmail.com>
2026-04-10 02:26:52 +0530

    feat: update celery configuration with beat schedule for reminders and reports

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -2,21 +2,28 @@ from celery import Celery
 from app import create_app
 import time
 
+from celery.schedules import crontab
+
 celery = Celery(
     'tasks',
     broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0',
+    include=['tasks']
 )
 
 celery.conf.update(
     timezone='Asia/Kolkata',
     enable_utc=False,
+    beat_schedule={
+        'daily-interview-reminders': {
+            'task': 'tasks.send_daily_reminders',
+            'schedule': crontab(hour=2, minute=21), 
+        },
+        'monthly-placement-reports': {
+            'task': 'tasks.generate_monthly_report',
+            'schedule': crontab(day_of_month='10', hour=2, minute=22), 
+        }
+    }
 )
 
 flask_app = create_app()
-
-@celery.task()
-def example_task():
-    with flask_app.app_context():
-        print("Executing example task...")
-        time.sleep(5)
-        print("Example task completed.")
